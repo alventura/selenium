bc5048c42d3223d2d0267938e97dce7d4b65bce4
alventura <alventura21@gmail.com>
2019-05-30 18:19:09 -0400

    Group HW

--- src/com/class10/HwIndividual.java
+++ src/com/class10/HwIndividual.java
@@ -1,5 +1,41 @@
 package com.class10;
 
-public class HwIndividual {
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import utils.CommonMethods;
+
+public class HwIndividual extends CommonMethods {
+public static void main(String[] args) {
+	setUpDriver("chrome","https://www.jqueryui.com/");
+	clicking("xpath","//a[@href='https://jqueryui.com/datepicker/']");
+	
+	WebElement element =driver.findElement(By.cssSelector("iframe.demo-frame"));
+	driver.switchTo().frame(element);
+	clicking("xpath","//input[@id='datepicker']");
+	
+	String expected="August 2019";
+	
+	//clicking("xpath","//a[@data-handler='next']");
+	String monthText=driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div/div")).getText();
+	String  expectedDay = "10";
+	
+	clicking("xpath","//a[@data-handler='next']");
+	clicking("xpath","//a[@data-handler='next']");
+	clicking("xpath","//a[@data-handler='next']");
+	 List<WebElement> table = driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']/tbody/tr/td"));
+	 for (WebElement day : table) {
+    	 String dayText = day.getText();
+    	 System.out.println(dayText);
+	if(dayText.equals(expectedDay)) {
+	               day.click();
+	        }else { 
+	        System.out.println("oh no!");
+	        }
+	
+		
+		}
+}
 }
